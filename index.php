2697c2d665d2b72b147084b9d52fe54dea72efd9
Andreas Harasztosi <aph@gmx.at>
2019-08-07 15:51:16 +0200

    repair 07-08-2019

--- index.php
+++ index.php
@@ -12,6 +12,29 @@
   <body>
     <div class="container">
       <h1 class="text-center text-danger">Big Library</h1>
+      <?php
+
+        $status = array('avail', 'reserved');
+
+        if (isset($_POST['edit']) && !empty($_POST['edit'])) {
+         echo
+         '
+          <form action="" method="post">
+            <input type="text" name="edit_isbn" value="'.$row['isbn'].'">
+            <input type="text" name="edit_title" value="'.$row['title'].'">
+            <select name="edit_type">
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+            <select name="edit_status">
+              <option value="'.$row['status'].'">'.$status[$row['status']].'</option>
+              <option value=""></option>
+            </select>
+          </form>
+        ';
+        }
+        
+      ?>
       <table class="table table-dark">
         <thead>
           <tr>
@@ -20,15 +43,14 @@
             <!--<th scope="col">Author</th>-->
             <th scope="col">Type</th>
             <th scope="col">Status</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
           <?php
             $bigLibrary = new mysqli('localhost', 'root', '', 'cr10_andreas_harasztosi_biglibrary');
-            //$sql = 'SELECT isbn, title, type, status, name, surname FROM media JOIN LEFT author ON media.fk_author = author.authorID;';
-            $sql = 'SELECT isbn, title, type, status FROM media;';
-            $status = array('avail', 'reserved');
-
+            /*$sql = 'SELECT isbn, title, type, status, name, surname FROM media LEFT JOIN author ON media.fk_author = author.authorID;';*/
+            $sql = 'SELECT mediaID, isbn, title, type, status FROM media;';
             $result = $bigLibrary->query($sql);
 
             if ($result->num_rows > 0) {
@@ -38,6 +60,10 @@
                   <td>'.$row['title'].'</td>
                   <td>'.$row['type'].'</td>
                   <td>'.$status[$row['status']].'</td>
+                  <td>
+                    <button type="submit" name="edit" value='.$row['mediaID'].'" class="btn btn-warning">Edit</a>
+                    <button type="submit" name="delete" value="'.$row['mediaID'].'" class="btn btn-danger">Delete</button>
+                  </td>
                 </tr>';
               }
             } else {
